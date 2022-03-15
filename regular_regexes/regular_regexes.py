44e06c79e93b3aa45e914914035bcea9b96723b0
Tijs G <tijs.gommeren@gmail.com>
2022-03-15 22:13:21 +0100

    loading regexes from external json file

--- regular_regexes/regular_regexes.py
+++ regular_regexes/regular_regexes.py
@@ -1,8 +1,12 @@
 import re
+import json
+
+regexes_file = open('../regexes/regexes.json')
+regexes_dict = json.load(regexes_file)
 
 
 class rre():
     def email(input):
-        email_regex = "[^@]+@[^@]+\.[^@]+"
+        email_regex = regexes_dict['email']
         result = re.findall(email_regex, input)
         return result
