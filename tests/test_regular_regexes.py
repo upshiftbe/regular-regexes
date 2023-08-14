0fe16798c706744e44f8c9bc00dc6d171f7d439b
tijsg <tijs.gommeren@gmail.com>
2023-08-14 17:22:03 +0200

    added check for multiple consecutive punctuation

--- tests/test_regular_regexes.py
+++ tests/test_regular_regexes.py
@@ -9,3 +9,11 @@ def test_url():
     assert len(rre.url("check it at testcom")) == 0
     assert len(rre.url("check it at https://test.com")) > 0
     assert len(rre.url("check it at https://testcom")) == 0
+
+def test_multiple_punctuation():
+    assert len(rre.multiple_punctuation("how are you??")) > 0
+    assert len(rre.multiple_punctuation("how are you?!")) > 0
+    assert len(rre.multiple_punctuation("&&&&& that's how it is")) > 0
+    assert len(rre.multiple_punctuation("& that's how it is")) == 0
+    assert len(rre.multiple_punctuation("how are you?")) == 0
+    assert len(rre.multiple_punctuation("look over there!")) == 0
