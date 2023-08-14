8f98ffd9ef9e1e92861179e5821dd73ab4d92d13
tijsg <tijs.gommeren@gmail.com>
2023-08-14 16:10:39 +0200

    added URL regex

--- tests/test_regular_regexes.py
+++ tests/test_regular_regexes.py
@@ -2,3 +2,10 @@ from regular_regexes.regular_regexes import rre
 
 def test_email():
     assert len(rre.email("test@test.com")) > 0
+    assert len(rre.email("test@test")) == 0
+
+def test_url():
+    assert len(rre.url("check it at test.com")) > 0
+    assert len(rre.url("check it at testcom")) == 0
+    assert len(rre.url("check it at https://test.com")) > 0
+    assert len(rre.url("check it at https://testcom")) == 0
